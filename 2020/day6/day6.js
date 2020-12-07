975d0b34dbb5180b749c160f97ab79277e66cb32
Maisam Somji <maisam.somji@gmail.com>
2020-12-06 21:58:59 -0500

    2020:6 - refactor

--- 2020/day6/day6.js
+++ 2020/day6/day6.js
@@ -1,56 +1,33 @@
 const fs = require('fs');
 
 const p1 = input => {
-  return input.reduce((total, group) => {
-    counts = group.split('').reduce((obj, letter) => {
-      obj[letter] = 1
-      return obj
-    }, {})
-    return Object.keys(counts).length + total
-  }, 0)
+  return input
+    .map(a => a.replace(/\n/g, ""))
+    .map(a => new Set(a.split('')))
+    .map(s => s.size)
+    .reduce((a, s) => a + s, 0)
+
 }
 
-const buildAlphaObject = string => 
-   string.split("").reduce((acc, l) => {
-    acc[l] = acc[l] ? acc[l] + 1 : 1
-    return acc
-  }, {})
+const p2 = input =>
+  input
+    .map(x => x.split('\n'))
+    .map(group => {
+      firstPerson = group[0].split('')
+      commonAnswerWithFirstPerson = firstPerson.filter(answer => group.every(person => person.includes(answer)))
+      return commonAnswerWithFirstPerson
+    })
+    .map(s => s.length)
+    .reduce((a, s) => a + s, 0)
 
-const mergeObjects = (mergeTo, otherObject) => {
-  Object.keys(otherObject).forEach(key => {
-    mergeTo[key] = mergeTo[key] ? mergeTo[key] + otherObject[key] : otherObject[key]
-  })
-  return mergeTo
-}
-
-const p2 = input => {
-  return input.map(d => {
-    if ('string' == typeof d) {
-      return d.length
-    } else {
-      const groupLength = d[0].length
-      const objectsByLetterCount = d[0].map(buildAlphaObject)
-      const combined = objectsByLetterCount.reduce(mergeObjects, {})
-      const numOfAllYes = Object.keys(combined).reduce((sum, key) => combined[key] === groupLength ? sum + 1 : sum, 0)
-      return numOfAllYes
-    }
-  }).reduce((a, s) => a + s, 0)
-}
 
 fs.readFile('./input.txt', (err, data) => {
   if (err) throw err;
   const input1 = data.toString()
     .split('\n\n')
-    .map(a => a.replace(/\n/g, ""))
 
   const input2 = data.toString()
     .split('\n\n')
-    .map(a => {
-      if (a.includes('\n')) {
-        return [a.split(/\n/)]
-      }
-      return a
-    })
 
   answer1 = p1(input1)
   answer2 = p2(input2)
