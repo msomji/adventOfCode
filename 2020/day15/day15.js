25db494a2a013f7dc164b0807b4afb2e3908df18
Maisam Somji <maisam.somji@gmail.com>
2020-12-15 02:13:58 -0500

    2020:15 - clean up

--- 2020/day15/day15.js
+++ 2020/day15/day15.js
@@ -1,59 +1,49 @@
-const fs = require('fs');
-const { last } = require('lodash');
 
+const addTurn = (map, key, value) => {
+  if (map.has(key)) {
+    map.set(key, [Math.max(...map.get(key)), value])
+  } else {
+    map.set(key, [value])
+  }
+}
 const p1 = (input, itterations) => {
   memoryMap = new Map() // numSaid : [last 2 turns]
   let mostRecentNumber = 0
   let currentIndex = 0
   currentTurn = 1
   input.forEach((num, index) => {
-    if (memoryMap.has(num)) {
-      memoryMap.set(num, [Math.max(...memoryMap.get(num)), currentTurn])
-    } else {
-      memoryMap.set(num, [currentTurn])
-    }
+    addTurn(memoryMap, num, currentTurn)
+
     mostRecentNumber = num
     currentIndex = index
-    currentTurn +=1
-    itterations-=1
+    currentTurn += 1
+    itterations -= 1
   })
   while (itterations > 0) {
     if (memoryMap.has(mostRecentNumber) && memoryMap.get(mostRecentNumber).length > 1) {
       let last = memoryMap.get(mostRecentNumber)[memoryMap.get(mostRecentNumber).length - 1]
       let lastBfore = memoryMap.get(mostRecentNumber)[memoryMap.get(mostRecentNumber).length - 2]
       let age = last - lastBfore
-      if (memoryMap.has(age)) {
-        memoryMap.set(age, [Math.max(...memoryMap.get(age)), currentTurn])
 
-      } else {
-        memoryMap.set(age, [currentTurn])
-      }
+      addTurn(memoryMap, age, currentTurn)
+
       mostRecentNumber = age
 
     } else {
-      if (memoryMap.has(0)) {
-        memoryMap.set(0, [Math.max(...memoryMap.get(0)), currentTurn])
-
-      } else {
-        memoryMap.set(0, [currentTurn])
-      }
+      addTurn(memoryMap, 0, currentTurn)
       mostRecentNumber = 0
     }
     itterations -= 1
     currentIndex += 1
-    currentTurn +=1
+    currentTurn += 1
   }
-
   return mostRecentNumber
 }
 
-fs.readFile('./input.txt', (err, data) => {
-  if (err) throw err;
 
-  answer1 = p1([15,5,1,4,7,0], 2020)
-  console.log(`answer1: ${answer1}`)
-  answer2 = p1([15,5,1,4,7,0], 30000000)
-  console.log(`answer2: ${answer2}`)
+answer1 = p1([15, 5, 1, 4, 7, 0], 2020)
+console.log(`answer1: ${answer1}`)
 
+answer2 = p1([15, 5, 1, 4, 7, 0], 30000000)
+console.log(`answer2: ${answer2}`)
 
-})
